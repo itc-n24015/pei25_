c1fb0a1380688f59fdab3b4ebb380155c5ac038a
itc-n24015 <n24015@std.it-college.ac.jp>
2025-10-10 02:39:27 +0000

    10-10

---
+++ m1_q3_kadai.py
@@ -0,0 +1,11 @@
+a = 12
+b = 5
+print(b - a) # (12)
+print(abs(-7)) # (13)
+print("{0:008b}".format(64)) # (14)
+c = [2, 7, 15, 12, 9]
+C.sort(reverse=True)
+print(c)
+print(c[1:4])
+print(f'((a - b) / c[3]:_<8.1f}')
+
